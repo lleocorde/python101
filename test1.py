0ca14ac6c06734443b5beac92e0a370217d1c42b
lleocorde <32246923+lleocorde@users.noreply.github.com>
2019-06-08 23:03:42 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
-name = input('What is your name?\n')
-age = int(input('How old are you? '))
+name = input("What is your name?\n")
+age = int(input("How old are you? "))
 print(type(name))
 print(type(age))
 print("Your name is %s and you are %d years old" % (name,age))
